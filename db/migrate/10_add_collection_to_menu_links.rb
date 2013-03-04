7238aacba18bbc1eb26d31895f3efbd3db6aa8f8
Rob Holland <rob@initforthe.com>
2013-03-04 17:44:28 +0000

    Fix table name.

--- db/migrate/10_add_collection_to_menu_links.rb
+++ db/migrate/10_add_collection_to_menu_links.rb
@@ -1,5 +1,5 @@
 class AddCollectionToMenuLinks < ActiveRecord::Migration
   def change
-    add_column :refinery_menu_links, :collection, :string
+    add_column :refinery_menus_links, :collection, :string
   end
 end
