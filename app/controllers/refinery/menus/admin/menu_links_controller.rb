eba1f88a54ce5e8105ff0f553f6095737e086314
Rob Holland <rob@initforthe.com>
2013-03-04 15:50:31 +0000

    Whitespace.

--- app/controllers/refinery/menus/admin/menu_links_controller.rb
+++ app/controllers/refinery/menus/admin/menu_links_controller.rb
@@ -7,7 +7,9 @@ module Refinery
           respond_to do |format|
             format.js do
               @menu_links = []
-              if params[:refinery_resource_ids]
+              if params[:collection].present?
+
+              elsif params[:refinery_resource_ids]
                 params[:refinery_resource_ids].each do |id|
                   @menu_links << MenuLink.create({refinery_resource_id: id}.merge(params[:menu_link]))
                 end
